f5681673189f4be9d2a0d00d171c29a9aba5860c
Deep Darji <darjideep74@gmail.com>
2025-05-08 14:26:39 +0530

    commmit0.1

--- lib/core/constants.dart
+++
@@ -1,9 +0,0 @@
-class Constants {
-  static const List<int> timerOptions = [10, 20, 30, 40, 50, 60];
-  static const List<String> musicTracks = [
-    'forest.mp3',
-    'rain.mp3',
-    'waves.mp3',
-    'wind.mp3',
-  ];
-}
